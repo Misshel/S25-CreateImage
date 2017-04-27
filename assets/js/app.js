d1c5f2df881f6e8afbbee8cfeb572242732834aa
Misshel <jenmish094@gmail.com>
2017-04-27 17:48:49 -0500

    ejercio de menu en imagen v1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,23 @@
+
+// ---------modificando para ayudantia
 function creandoDiv(getURL) {
   var contenedor= document.getElementById("contenedor");
   var div = document.createElement("div");
+  var divMenu = document.createElement("div");
+  var label1= document.createElement("label");
+              label1.innerText= "Whith: ";
+  var label2= document.createElement("label");
+              label2.innerText= "Height: ";
+
+  var input= document.createElement("input");
+divMenu.appendChild(label1);
+divMenu.appendChild(input);
+divMenu.appendChild(label2);
+divMenu.appendChild(input);
+
   var img= document.createElement("img");
   img.setAttribute("src", getURL);
   div.classList.add("contenedor-imagen");
   div.appendChild(img);
   contenedor.appendChild(div);
-}
-
-var inputURL= document.getElementById("url");
-
-function getURL(e){
-  if (e.keyCode == 13) {
-    creandoDiv(this.value);
-      this.value = " ";
-  }
-}
-
-inputURL.onkeydown = getURL;
+  contenedor.appendChild(divMenu);
